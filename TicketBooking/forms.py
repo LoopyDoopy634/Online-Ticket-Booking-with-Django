6d1f0eece1d666d75b054e87968dc2efb11fc450
LoopyDoopy634 <loopydoopy634@gmail.com>
2024-06-23 15:24:07 +0530

    Added buttons to all pages and modified the look plus a message for booking success

--- TicketBooking/forms.py
+++ TicketBooking/forms.py
@@ -5,3 +5,13 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        self.event = kwargs.pop('event', None)
+        super().__init__(*args, **kwargs)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Ticket.objects.filter(event=self.event, email=email).exists():
+            raise forms.ValidationError("You have already booked a ticket for this event with this email.")
+        return email
--- TicketBooking/views.py
+++ TicketBooking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Event, Ticket
 from .forms import TicketForm
+from django.contrib import messages
 
 def event_list(request):
     events = Event.objects.all()
@@ -9,14 +10,15 @@ def event_list(request):
 def book_ticket(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     if request.method == 'POST':
-        form = TicketForm(request.POST)
+        form = TicketForm(request.POST, event=event)
         if form.is_valid():
             ticket = form.save(commit=False)
             ticket.event = event
             ticket.save()
-            return redirect('event_list')
+            messages.success(request, 'Ticket booked successfully!')
+            form = TicketForm(event=event)  # Clear the form
     else:
-        form = TicketForm()
+        form = TicketForm(event=event)
     return render(request, 'book_ticket.html', {'form': form, 'event': event})
 
 def event_bookings(request, event_id):
